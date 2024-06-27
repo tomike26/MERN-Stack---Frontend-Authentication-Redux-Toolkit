748d177cf208c919a5a602f091224f31371371c9
tomike26 <kolajotomike26@gmail.com>
2024-06-26 23:04:38 -0500

    folder-structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,11 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
-import Header from './components/Header'
-import Dashboard from './pages/Dashboard'
-import Login from './pages/Login'
-import Register from './pages/Register'
+
 
 function App() {
   return (
-    <>
-      <Router>
-        <div className='container'>
-          <Header />
-          <Routes>
-            <Route path='/' element={<Dashboard />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/register' element={<Register />} />
-          </Routes>
-        </div>
-      </Router>
-      <ToastContainer />
-    </>
-  )
+    <div className="App">
+      <h1>My App</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,10 +1,8 @@
-import { configureStore } from '@reduxjs/toolkit'
-import authReducer from '../features/auth/authSlice'
-import goalReducer from '../features/goals/goalSlice'
+import { configureStore } from '@reduxjs/toolkit';
+import counterReducer from '../features/counter/counterSlice';
 
 export const store = configureStore({
   reducer: {
-    auth: authReducer,
-    goals: goalReducer,
+    counter: counterReducer,
   },
-})
+});
