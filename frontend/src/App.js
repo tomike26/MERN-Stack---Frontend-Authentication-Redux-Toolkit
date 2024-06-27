5a91ca98681466d27c9e45248dd66bb8de0837bc
tomike26 <kolajotomike26@gmail.com>
2024-06-27 14:02:19 -0500

    registration and login page fixed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,26 @@
-
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <h1>My App</h1>
+    <>
+    <Router>
+    <div className="" >
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Dashboard/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/register' element={<Register/>}/>
+      </Routes>
+
     </div>
+    </Router>
+   
+    </>
+    
   );
 }
 
